ed5b8606022c54411b18b525cb17f234dfa306b7
Sophia Li <sophiali124@gmail.com>
2020-05-27 16:45:44 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = require('./routes')
+
+const app = express()
+const PORT = process.env.PORT || 3005
+
+app.use(express.json())
+app.use('/api', router)
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `)
+})
+
+module.exports = app
